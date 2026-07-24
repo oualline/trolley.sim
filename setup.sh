4ef6a7e321c8dd65b5a6dc3586f233c94afa427c
Steve Oualline <oualline@gmail.com>
2026-07-24 16:46:48 -0700

    Fixed brake arrow

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-sudo apt install vim-gtk3
+sudo apt install vim-gtk3 git
 sudo apt-get install default-jre libreoffice-java-common
 sudo apt install python3-pip
 sudo apt install python3-pynput
@@ -10,8 +10,6 @@ sudo apt install pyqt6-dev-tools python3-pyqt6
 sudo apt install designer-qt6
 sudo apt install qmake6
 
-echo "/usr/lib/qt6/bin" | sudo tee /usr/lib/x86_64-linux-gnu/qtchooser/qt6.conf
-echo "/usr/lib/x86_64-linux-gnu" | sudo tee -a /usr/lib/x86_64-linux-gnu/qtchooser/qt6.conf
 
 # For mpv video playback
 sudo apt install libmpv-dev
@@ -21,3 +19,13 @@ sudo pip install \
     playsound3 \
     pyinstaller \
     --break-system-packages
+
+
+sudp apt install qtchooser
+sudo apt install python3-pyqt6.qtsvg
+echo "/usr/lib/qt6/bin" | sudo tee /usr/lib/x86_64-linux-gnu/qtchooser/qt6.conf
+echo "/usr/lib/x86_64-linux-gnu" | sudo tee -a /usr/lib/x86_64-linux-gnu/qtchooser/qt6.conf
+
+DIR=/usr/lib/x86_64-linux-gnu/qtchooser/
+#sudo sed -e 's/5/6/g' $DIR/qt5.conf >$DIR/qt6.conf
+sudo ln $DIR/qt6.conf $DIR/default.conf
